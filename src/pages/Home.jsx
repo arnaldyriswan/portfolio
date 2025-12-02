9e3b84d821fb9e56151b2a96dbcd0ee6412769a9
Riz <123041516+Arizh2005@users.noreply.github.com>
2025-12-02 12:32:49 +0700

    :refactor index.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export const Home = () => {
     return (
-        <div className="min-h-screen container">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
         </div>
     )
